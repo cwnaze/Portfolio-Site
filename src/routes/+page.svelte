fe988e2a079039a6a4fc219f0c311f9afb3ea4b0
Casey Nazelrod <cwnaze@gmail.com>
2024-11-12 14:46:05 -0500

    feat: copyright section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,4 @@
-<h1 class='text-9xl h-svh flex justify-center items-center'>Coming Soon</h1>
+<div class='h-svh flex justify-center items-center flex-col'>
+    <h1 class='text-9xl'>Coming Soon</h1>
+    <p class='text-2xl mt-4'>&#169; Casey Nazelrod 2024</p>
+</div>
