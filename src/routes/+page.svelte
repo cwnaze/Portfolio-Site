d4af9bc49ec16c0ca3e68d8e0da9f86695976328
Casey Nazelrod <cwnaze@gmail.com>
2024-11-12 15:05:25 -0500

    style: text center

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <div class='h-svh flex justify-center items-center flex-col'>
-    <h1 class='text-9xl'>Coming Soon</h1>
-    <p class='text-2xl mt-4'>&#169; Casey Nazelrod 2024</p>
+    <h1 class='text-9xl text-center'>Coming Soon</h1>
+    <p class='text-2xl mt-4 text-center'>&#169; Casey Nazelrod 2024</p>
 </div>
