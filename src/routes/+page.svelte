6950feccfff9b69ff87b2b936244825b0d2621b7
Casey Nazelrod <cwnaze@gmail.com>
2024-11-13 08:04:40 -0500

    chore: update copyright format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <div class='h-svh flex justify-center items-center flex-col'>
     <h1 class='text-9xl text-center'>Coming Soon</h1>
-    <p class='text-2xl mt-4 text-center'>&#169; Casey Nazelrod 2024</p>
+    <p class='text-2xl mt-4 text-center'>Copyright &#169; 2024 Casey Nazelrod</p>
 </div>
