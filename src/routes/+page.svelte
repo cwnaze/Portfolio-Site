498e839c43b68d02e5927c0ad6b5084575a02044
Casey Nazelrod <cwnaze@gmail.com>
2024-11-16 16:23:56 -0500

    feat: landing page graphic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,25 @@
-<div class='h-svh flex justify-center items-center flex-col'>
-    <h1 class='text-9xl text-center'>Coming Soon</h1>
-    <p class='text-2xl mt-4 text-center'>Copyright &#169; 2024 Casey Nazelrod</p>
-</div>
+<body>
+    <div class='flex items-center justify-center h-screen'>
+        <svg width=750 height=1000 class='absolute'>
+            <path id='curve' d='M50,500 a1,1 0 0,0 650,0' fill='none' stroke='none' />
+            <text>
+                <textPath xlink:href='#curve' startOffset='5%' dominant-baseline='hanging' class='text-4xl font-semibold stroke-blue-100 fill-blue-100 tracking-wider'>
+                    About Me
+                </textPath>
+                <textPath xlink:href='#curve' startOffset='26%' dominant-baseline='hanging' class='text-5xl font-black stroke-green-100 fill-green-100'>
+                    &bull;
+                </textPath>
+                <textPath xlink:href='#curve' startOffset='32%' dominant-baseline='hanging' class='text-4xl font-semibold stroke-blue-100 fill-blue-100 tracking-wider'>
+                    Software Developer
+                </textPath>
+                <textPath xlink:href='#curve' startOffset='71%' dominant-baseline='hanging' class='text-5xl font-black stroke-green-100 fill-green-100'>
+                    &bull;
+                </textPath>
+                <textPath xlink:href='#curve' startOffset='75%' dominant-baseline='hanging' class='text-4xl font-semibold stroke-blue-100 fill-blue-100 tracking-wider'>
+                    Cybersecurity
+                </textPath>
+            </text>
+        </svg>
+        <img src='/images/logo.svg' alt='Casey Nazelrod Logo'>
+    </div>
+</body>
