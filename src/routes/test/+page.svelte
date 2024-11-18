f277c638e787318a7ef428abe51793933b8da10f
Casey Nazelrod <cwnaze@gmail.com>
2024-11-18 10:40:43 -0500

    chore: remove test

--- src/routes/test/+page.svelte
+++
@@ -1,8 +0,0 @@
-<div class='flex justify-between p-5 bg-yellow-100 items-center drop-shadow-md'>
-    <img src="/images/logo.svg" alt="Casey Nazelrod Logo" width='75'>
-    <ul class='flex gap-10'>
-        <li class='text-3xl font-semibold text-blue-100'>About Me</li>
-        <li class='text-3xl font-semibold text-blue-100'>Software Developer</li>
-        <li class='text-3xl font-semibold text-blue-100'>Cybersecurity</li>
-    </ul>
-</div>
