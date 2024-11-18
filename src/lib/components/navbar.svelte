9dfa74c35fa12ab09a7c4982606ce71f55952dd4
Casey Nazelrod <cwnaze@gmail.com>
2024-11-18 10:40:24 -0500

    feat: transition after hero

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,5 +1,5 @@
 <div class='flex justify-between p-5 bg-yellow-100 items-center drop-shadow-md'>
-    <img src="/images/logo.svg" alt="Casey Nazelrod Logo" width='75'>
+    <img src="/images/logo.svg" alt="Casey Nazelrod Logo" width='50'>
     <ul class='flex gap-10'>
         <li class='text-3xl font-semibold text-blue-100'>About Me</li>
         <li class='text-3xl font-semibold text-blue-100'>Software Developer</li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
 <script lang='ts'>
+    import { gsap } from 'gsap';
+    import Navbar from '$lib/components/navbar.svelte';
     let innerWidth = $state(0);
     let innerHeight = $state(0);
-    let scrollY = 0;
-    let aboutMeOffset = $state(1);
-    let bull1Offset = $state(8);
-    let softwareOffset = $state(10);
-    let bull2Offset = $state(23);
-    let cyberOffset = $state(25);
-
+    let aboutMe: SVGTextPathElement;
+    let software: SVGTextPathElement;
+    let cyber: SVGTextPathElement;
+    let hero: HTMLElement;
     let bull1: SVGTextPathElement;
     let bull2: SVGTextPathElement;
+    let logo: HTMLImageElement;
+    let navbar: HTMLElement;
+    let heroAnimComplete: boolean = false;
 
     const updateDimensions = () => {
         if (typeof window !== 'undefined') {
@@ -20,64 +22,149 @@
 
     function handleHeroScroll(event: Event) {
         event.preventDefault();
-        scrollY = window.scrollY;
-        if(scrollY != 0) {
-            if(aboutMeOffset == 1 && scrollY/Math.abs(scrollY) == -1) return
-            if(cyberOffset == 91 && scrollY/Math.abs(scrollY) == 1) {
-                aboutMeOffset
-                bull1.style.display = 'none';
-                bull2.style.display = 'none';
-                softwareOffset = 78;
-                aboutMeOffset = 71;
+        const { deltaY } = event as WheelEvent;
+        console.log(aboutMe.startOffset.baseVal.valueInSpecifiedUnits)
+        if(deltaY != 0) {
+            console.log(aboutMe.startOffset.baseVal.valueInSpecifiedUnits)
+            if(aboutMe.startOffset.baseVal.valueInSpecifiedUnits <= 1 && deltaY/Math.abs(deltaY) == -1) {
+                gsap.to(aboutMe, {
+                    duration: 0.1,
+                    attr: {startOffset: '1%'}
+                })
+                gsap.to(bull1, {
+                    duration: 0.1,
+                    attr: {startOffset: '8%'}
+                })
+                gsap.to(software, {
+                    duration: 0.1,
+                    attr: {startOffset: '10%'}
+                })
+                gsap.to(bull2, {
+                    duration: 0.1,
+                    attr: {startOffset: '23%'}
+                })
+                gsap.to(cyber, {
+                    duration: 0.1,
+                    attr: {startOffset: '25%'}
+                })
                 return
             }
+            if(cyber.startOffset.baseVal.valueInSpecifiedUnits >= 91 && deltaY/Math.abs(deltaY) == 1) {
+                bull1.style.display = 'none';
+                bull2.style.display = 'none';
+                gsap.to(cyber, {
+                    duration: 0.1,
+                    attr: {startOffset: '91%'}
+                })
+                gsap.to(software, {
+                    duration: 0.1,
+                    attr: {startOffset: '78%'}
+                })
+                gsap.to(aboutMe, {
+                    duration: 0.1,
+                    attr: {startOffset: '71%'}
+                })
+                var tl = gsap.timeline();
+                tl.to(logo, {
+                    duration: 0.3,
+                    attr: {width: 50, top: '0px', left: '0px'}
+                })
+                tl.to(hero, {
+                    duration: 0.2,
+                    opacity: 0,
+                    ease: "power2.out",
+                    onComplete: () => {
+                        hero.style.display = 'none';
+                        gsap.to(navbar, {
+                            duration: 0.2,
+                            display: 'block',
+                            opacity: 1,
+                            ease: "power2.in"
+                        })
+                    }
+                })
+                logo.style.top = '0px'
+                logo.style.left = '0px'
+                heroAnimComplete = true;
+                document.body.style.overflow = 'visible';
+                window.removeEventListener('wheel', handleHeroScroll);
+                window.removeEventListener('touchmove', handleHeroScroll);
+
+                return
+            }
+            gsap.to(logo, {
+                duration: 0.3,
+                attr: {width: 600, top: `${innerHeight - 300}px`, left: `${innerWidth + 25}px`}
+            })
+            logo.width = 600;
+            logo.height = 600;
+            logo.style.top = `${innerHeight - 300}px`
+            logo.style.left = `${innerWidth + 25}px`
             bull1.style.display = 'block';
             bull2.style.display = 'block';
-            bull1Offset = bull1Offset + (3 * (scrollY/Math.abs(scrollY)));
-            bull2Offset = bull2Offset + (3 * (scrollY/Math.abs(scrollY)));
-            cyberOffset = cyberOffset + (3 * (scrollY/Math.abs(scrollY)));
-            softwareOffset = cyberOffset - 15;
-            aboutMeOffset = softwareOffset - 9;
-            console.log(cyberOffset);
-        }
-        
+            gsap.to(bull1, {
+                duration: 0.1,
+                attr: {startOffset: `${bull1.startOffset.baseVal.valueInSpecifiedUnits + (3 * (deltaY/Math.abs(deltaY)))}%`}
+            })
+            gsap.to(bull2, {
+                duration: 0.1,
+                attr: {startOffset: `${bull2.startOffset.baseVal.valueInSpecifiedUnits + (3 * (deltaY/Math.abs(deltaY)))}%`}
+            })
+            gsap.to(cyber, {
+                duration: 0.1,
+                attr: {startOffset: `${cyber.startOffset.baseVal.valueInSpecifiedUnits + (3 * (deltaY/Math.abs(deltaY)))}%`}
+            })
+            gsap.to(software, {
+                duration: 0.1,
+                attr: {startOffset: `${software.startOffset.baseVal.valueInSpecifiedUnits + (3 * (deltaY/Math.abs(deltaY)))}%`}
+            })
+            gsap.to(aboutMe, {
+                duration: 0.1,
+                attr: {startOffset: `${aboutMe.startOffset.baseVal.valueInSpecifiedUnits + (3 * (deltaY/Math.abs(deltaY)))}%`}
+            })
+        }    
     }
 
     // Update dimensions on component mount
     if (typeof window !== 'undefined') {
         updateDimensions();
         window.addEventListener('resize', updateDimensions);
-        window.addEventListener('scroll', handleHeroScroll, {passive: false});
+        window.addEventListener('wheel', handleHeroScroll, {passive: false});
+        window.addEventListener('touchmove', handleHeroScroll, {passive: false});
+        if(!heroAnimComplete) document.body.style.overflow = 'hidden';
     }
 </script>
-    <div class='flex items-center justify-center h-screen'>
+    <div bind:this={hero} class='h-screen'>
         <svg class='absolute w-screen h-screen'>
             <path id='curve' d='M{innerWidth},{innerHeight} 
         a1,1 0 0,0 650,0
         a325,325 1 0,0 -325,-325
-        a325,325 0 0,0 -253,120.5
-        a100,100 0 0,1 0,-200 
+        a325,325 0 0,0 -253,125.5
+        a100,100 0 0,1 0,-{innerHeight - 210}
         H{innerWidth*2+650}' fill='none' stroke='none'/>
             <text>
-                <textPath xlink:href='#curve' startOffset='{aboutMeOffset}%' dominant-baseline='hanging' class='text-4xl font-semibold stroke-blue-100 fill-blue-100 tracking-wider'>
+                <textPath bind:this={aboutMe} xlink:href='#curve' startOffset='1%' dominant-baseline='hanging' class='text-4xl font-semibold stroke-blue-100 fill-blue-100 tracking-wider'>
                     About Me
                 </textPath>
-                <textPath bind:this={bull1} xlink:href='#curve' startOffset='{bull1Offset}%' dominant-baseline='hanging' class='text-5xl font-black stroke-green-100 fill-green-100'>
+                <textPath bind:this={bull1} xlink:href='#curve' startOffset='8%' dominant-baseline='hanging' class='text-5xl font-black stroke-green-100 fill-green-100'>
                     &bull;
                 </textPath>
-                <textPath xlink:href='#curve' startOffset='{softwareOffset}%' dominant-baseline='hanging' class='text-4xl font-semibold stroke-blue-100 fill-blue-100 tracking-wider'>
+                <textPath bind:this={software} xlink:href='#curve' startOffset='10%' dominant-baseline='hanging' class='text-4xl font-semibold stroke-blue-100 fill-blue-100 tracking-wider'>
                     Software Developer
                 </textPath>
-                <textPath bind:this={bull2} xlink:href='#curve' startOffset='{bull2Offset}%' dominant-baseline='hanging' class='text-5xl font-black stroke-green-100 fill-green-100'>
+                <textPath bind:this={bull2} xlink:href='#curve' startOffset='23%' dominant-baseline='hanging' class='text-5xl font-black stroke-green-100 fill-green-100'>
                     &bull;
                 </textPath>
-                <textPath xlink:href='#curve' startOffset='{cyberOffset}%' dominant-baseline='hanging' class='text-4xl font-semibold stroke-blue-100 fill-blue-100 tracking-wider'>
+                <textPath bind:this={cyber} xlink:href='#curve' startOffset='25%' dominant-baseline='hanging' class='text-4xl font-semibold stroke-blue-100 fill-blue-100 tracking-wider'>
                     Cybersecurity
                 </textPath>
             </text>
         </svg>
-        <img src='/images/logo.svg' alt='Casey Nazelrod Logo'>
+        <img bind:this={logo} width='600' height='600' style='position:absolute; right:{innerWidth+25}px; top:{innerHeight - 300}px' src='/images/logo.svg' alt='Casey Nazelrod Logo'>
     </div>
-
+    
+    <div bind:this={navbar} class='hidden'><Navbar/>
+        <h1 class='text-9xl text-center text-blue-100 mt-5 font-black'>Coming Soon</h1>
+    </div>
 
   
