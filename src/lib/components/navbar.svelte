10ca60fcfff0fb58be78fd99d61613c32ce48c08
Casey Nazelrod <cwnaze@gmail.com>
2024-11-21 20:55:24 -0500

    style: navbar hover anim

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,8 +1,14 @@
 <div class='flex justify-between p-5 bg-yellow-100 items-center drop-shadow-md'>
-    <img src="/images/logo.svg" alt="Casey Nazelrod Logo" width='50'>
+    <img src='/images/logo.svg' alt='Casey Nazelrod Logo' width='50'>
     <ul class='flex gap-10'>
-        <li class='text-3xl font-semibold text-blue-100'>About Me</li>
-        <li class='text-3xl font-semibold text-blue-100'>Software Developer</li>
-        <li class='text-3xl font-semibold text-blue-100'>Cybersecurity</li>
+        <li class='text-3xl font-semibold text-blue-100'><a href='#' class='relative transition-all duration-300 hover:text-white after:content-[""] after:absolute after:bg-[#E07A5F] after:h-[5px] after:w-0 after:left-0 after:-bottom-[7px] after:transition-all after:duration-300 hover:after:w-full active:text-white active:after:w-full'>
+            About Me
+        </a></li>
+        <li class='text-3xl font-semibold text-blue-100'><a href='#' class='relative transition-all duration-300 hover:text-white after:content-[""] after:absolute after:bg-[#E07A5F] after:h-[5px] after:w-0 after:left-0 after:-bottom-[7px] after:transition-all after:duration-300 hover:after:w-full active:text-white active:after:w-full'>
+            Software Developer
+        </a></li>
+        <li class='text-3xl font-semibold text-blue-100'><a href='#' class='relative transition-all duration-300 hover:text-white after:content-[""] after:absolute after:bg-[#E07A5F] after:h-[5px] after:w-0 after:left-0 after:-bottom-[7px] after:transition-all after:duration-300 hover:after:w-full active:text-white active:after:w-full'>
+            Cybersecurity
+        </a></li>
     </ul>
 </div>
